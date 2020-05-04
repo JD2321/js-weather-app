c556f26d86f62a61f6da5f30269187e3fb95deed
Jordan Diaz <JordanDiaz@Jordans-MacBook-Pro.local>
2020-05-04 18:11:56 -0400

    created API link and stared JS. Needs finished JS logic and css clean up

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
-console.log('ayyyeeeeeee');
+const api = {
+    key: "bce30d2d548f76cdea1803e470cdefb1",
+    baseurl: "http://api.openweathermap.org/data/2.5/"
+}
+
+const searchBar = document.querySelector(".search-bar");
+searchBar.addEventListener("keypress", setQuery);
+
+function setQuery(e) {
+    if (e.keyCode == 13) {
+        getResults(searchBar.value);
+        console.log(searchBar.value);
+    }
+};
+
+function getResults(query) {
+    fetch(`${api.baseurl}weather?q=${query}&units=imperial&APPID=${api.key}`)
+        .then(weather => {
+            return weather.json();
+        }).then(displayResults);
+}
+
+function displayResults(weather) {
+    console.log(weather);
+    let city = document.querySelector(".location .city");
+    city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+    //use date formatter instead of creating function
+    let now = new Date();
+    date = document.querySelector(".location .date");
+    date.innerText = dateBuilder(now);
+
+}
+
+function dateBuilder(d) {
+
+}
